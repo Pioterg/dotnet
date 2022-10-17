6a959ca464deca1d0d1a1b40e4753b37cf53e316
Piotr <piotrsitkowski@wp.pl>
2022-10-17 22:05:18 +0200

    add project

---
+++ Diary/Diary/ViewModels/AddEditStudentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Diary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diary.ViewModels
+{
+    internal class AddEditStudentViewModel : ViewModelBase
+    {
+        public AddEditStudentViewModel(Student student = null)
+        {
+
+        }
+    }
+}
