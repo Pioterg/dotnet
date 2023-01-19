c1f90cc8f0d11aa8dd4022f9cee5dac8a1a8b573
Piotr <piotrsitkowski@wp.pl>
2023-01-19 15:47:09 +0100

    Create web project

---
+++ InvoiceManager/InvoiceManager/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvoiceManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var test = "Test wffewfewfwef";
+
+            return View((object)test);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
